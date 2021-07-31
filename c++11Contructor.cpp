c65061cb53cef6a9ec75551667afc8cb6b69931c
yp <yinpan0227@hotmail.com>
2021-07-31 09:53:31 +0800

    c++11functional使用

--- c++11Contructor.cpp
+++ c++11Contructor.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <type_traits>
+#include <functional>
 using namespace std;
+
 class Base
 {
 public:
@@ -20,9 +22,65 @@ class Subclass : public Base
 public:
     using Base::Base; // 继承构造
 };
+
+class Magic
+{
+public:
+    Magic() = default;
+    Magic &operator=(const Magic &) = delete;
+    Magic(int magic_number);
+};
+
+enum class num : unsigned int
+{
+    value1,
+    value2 = 100
+};
+
+int foo(int para)
+{
+    return para;
+}
 int main()
 {
     Subclass s(3);
     std::cout << s.value1 << std::endl;
     std::cout << s.value2 << std::endl;
+
+    int value = 1;
+    auto copy_value = [=]
+    {
+        return value;
+    };
+
+    value = 100;
+    cout << copy_value() << endl;
+
+    // std::function 包装了一个返回值为 int, 参数为 int 的函数
+    std::function<int(int)> func = foo;
+
+    int important = 10;
+    std::function<int(int)> func2 = [&](int value) -> int
+    {
+        return 1 + value + important;
+    };
+    std::cout << func(10) << std::endl;
+    std::cout << func2(10) << std::endl;
 }
+
+// struct Base
+// {
+//     virtual void foo() final;
+// };
+// struct SubClass1 final : Base
+// {
+// }; // 合法
+
+// struct SubClass2 : SubClass1
+// {
+// }; // 非法, SubClass1 已 final
+
+// struct SubClass3 : Base
+// {
+//     void foo(); // 非法, foo 已 final
+// };
