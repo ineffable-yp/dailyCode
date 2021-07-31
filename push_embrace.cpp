af804623fdc2ad692809e0f260aeea0f16a61cf1
yp <yinpan0227@hotmail.com>
2021-07-31 14:26:41 +0800

    push-emplace

---
+++ push_embrace.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+class testDemo
+{
+public:
+    testDemo(int num) : num(num)
+    {
+        std::cout << "调用构造函数" << endl;
+    }
+    testDemo(const testDemo &other) : num(other.num)
+    {
+        std::cout << "调用拷贝构造函数" << endl;
+    }
+    testDemo(testDemo &&other) : num(other.num)
+    {
+        std::cout << "调用移动构造函数" << endl;
+    }
+
+private:
+    int num;
+};
+
+int main()
+{
+    cout << "emplace_back:" << endl;
+    std::vector<testDemo> demo1;
+    demo1.emplace_back(2);
+
+    cout << "push_back:" << endl;
+    std::vector<testDemo> demo2;
+    demo2.push_back(2);
+}
