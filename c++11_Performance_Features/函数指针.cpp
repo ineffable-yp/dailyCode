de1477af4de705299d57cb69bf0264c44bf3cc9c
yp <yinpan0227@hotmail.com>
2021-09-23 09:51:11 +0800

    函数指针地址

--- c++11_Performance_Features/函数指针.cpp
+++ c++11_Performance_Features/函数指针.cpp
@@ -11,6 +11,8 @@ void f()
 int main()
 {
     F f1 = f;
+    (*f1)();
+    //因为cout对象没有对函数指针作为输入参数的运算符<<重载
     cout << "f  : " << (void *)f << endl;
     cout << "&f : " << reinterpret_cast<void *>(&f) << endl;
     cout << "*f1: " << reinterpret_cast<void *>(*f1) << endl;
